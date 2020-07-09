603bf92fe6067c9e990b8a52efc6706bf0d0245e
diduhless <didacatz@hotmail.com>
2020-07-09 16:21:24 +0200

    Implement PartyEvent

---
+++ src/diduhless/parties/event/PartyEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace diduhless\parties\event;
+
+
+use pocketmine\event\Cancellable;
+use pocketmine\event\Event;
+
+abstract class PartyEvent extends Event implements Cancellable {
+
+}
