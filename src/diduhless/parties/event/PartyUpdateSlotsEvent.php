e8b072ebb7758341be9993b54e8c1d9cce044c1a
diduhless <didacatz@hotmail.com>
2020-07-12 23:52:28 +0200

    PartyUpdateSlotsEvent: slots can no longer be null

--- src/diduhless/parties/event/PartyUpdateSlotsEvent.php
+++ src/diduhless/parties/event/PartyUpdateSlotsEvent.php
@@ -9,19 +9,14 @@ use diduhless\parties\session\Session;
 
 class PartyUpdateSlotsEvent extends PartyEvent {
 
-    /** @var null|int */
+    /** @var int */
     private $slots;
 
-    public function __construct(Party $party, Session $session, ?int $slots) {
+    public function __construct(Party $party, Session $session, int $slots) {
         $this->slots = $slots;
         parent::__construct($party, $session);
     }
 
-    /**
-     * If the slots are set to null, it means there is no limit.
-     *
-     * @return int|null
-     */
     public function getSlots(): ?int {
         return $this->slots;
     }
