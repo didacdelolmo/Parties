0f06448bb8530744015806037c1acca3e8b8d192
diduhless <didacatz@hotmail.com>
2020-07-13 18:01:42 +0200

    Implement Invitation

--- src/diduhless/parties/party/Invitation.php
+++ src/diduhless/parties/party/Invitation.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace diduhless\parties\party;
 
 
+use diduhless\parties\event\PartyJoinEvent;
 use diduhless\parties\session\Session;
 
 class Invitation {
@@ -33,6 +34,27 @@ class Invitation {
 
     public function attemptToAccept(): void {
         $this->target->removeInvitation($this);
+
+        if($this->target->hasParty()) {
+            $this->target->message("{RED}You cannot be in a party to do this!");
+            return;
+        } elseif(!$this->sender->isOnline()) {
+            $this->target->message("{RED}You could not join the party because the party leader is not online!");
+            return;
+        }
+        $party = $this->sender->getParty();
+        if($party === null or $party->getId() !== $this->partyId) {
+            $this->target->message("{RED}You could not join the party because the party leader has left the party!");
+            return;
+        } elseif($party->isFull()) {
+            $this->target->message("{RED}You could not join this party because it is full!");
+        }
+
+        $event = new PartyJoinEvent($party, $this->target);
+        $event->call();
+        if(!$event->isCancelled()) {
+            $party->add($this->target);
+        }
     }
 
 }
