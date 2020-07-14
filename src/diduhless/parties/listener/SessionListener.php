8569b2653f87d83b2858b242c24ef0f8a698c056
diduhless <didacatz@hotmail.com>
2020-07-14 03:43:50 +0200

    Disband the party if the party leader disconnects

--- src/diduhless/parties/listener/SessionListener.php
+++ src/diduhless/parties/listener/SessionListener.php
@@ -6,6 +6,9 @@ declare(strict_types=1);
 namespace diduhless\parties\listener;
 
 
+use diduhless\parties\event\PartyDisbandEvent;
+use diduhless\parties\party\Party;
+use diduhless\parties\party\PartyFactory;
 use diduhless\parties\session\SessionFactory;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
@@ -27,8 +30,23 @@ class SessionListener implements Listener {
      * @priority LOWEST
      */
     public function onQuit(PlayerQuitEvent $event): void {
-        if(SessionFactory::hasSession($player = $event->getPlayer())) {
-            SessionFactory::removeSession($player);
+        $player = $event->getPlayer();
+        if(SessionFactory::hasSession($player)) {
+            $session = SessionFactory::getSession($player);
+            if($session->isPartyLeader()) {
+                $this->disbandParty($session->getParty());
+            }
+        }
+        SessionFactory::removeSession($player);
+    }
+
+    private function disbandParty(Party $party): void {
+        $event = new PartyDisbandEvent($party, $party->getLeader());
+        $event->call();
+
+        foreach($party->getMembers() as $member) {
+            $party->remove($member);
+            PartyFactory::removeParty($party);
         }
     }
 
