0a8cbf27fcf0e6326809685eed0018896bd7d8f3
diduhless <didacatz@hotmail.com>
2021-01-12 10:04:18 +0100

    Check if the player message belongs to the party chat

---
+++ src/diduhless/parties/listener/PartyChatListener.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace diduhless\parties\listener;
+
+
+use diduhless\parties\session\SessionFactory;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+
+class PartyChatListener implements Listener {
+
+    /**
+     * @param PlayerChatEvent $event
+     * @priority HIGHEST
+     */
+    public function onChat(PlayerChatEvent $event): void {
+        $player = $event->getPlayer();
+        if(!SessionFactory::hasSession($player)) {
+            return;
+        }
+
+        $session = SessionFactory::getSession($player);
+        if($session->hasPartyChat() and $session->hasParty()) {
+            $session->getParty()->sendColoredMessage($session, $event->getMessage());
+
+            $event->setCancelled();
+        }
+    }
+
+}
